a46192dbd4f45c9d7971eb9045c053ff6845e4df
Khachatur Azatyan <Khachatur.Azatyan@istc.com>
2019-02-07 19:20:29 +0400

    finished

--- homework1019/1019.js
+++ homework1019/1019.js
@@ -9,14 +9,14 @@ if(text){
        let mindiv = document.createElement("div");
        let note = document.getElementById("pad")
        let icheak = document.createElement("i");
-       icheak.className=("far fa-check-square fa-2x green-color");
+       icheak.className="far fa-check-square fa-2x";
        icheak.id=("cheakbox");
        icheak.addEventListener("click", changeColor);
        let itrash = document.createElement("i");
-       itrash.className=("far fa-trash-alt f2ed fa-2x");
+       itrash.className="far fa-trash-alt f2ed fa-2x";
        itrash.id=("trashbin");
-       itrash.addEventListener("click", delet)
-       mindiv.className=("todo");
+       itrash.addEventListener("click", delet);
+       mindiv.className=("green");
        let textdiv = document.createElement("div");
        textdiv.className=("divtext");
        let newtext = document.createTextNode(addArr[i]);
@@ -30,9 +30,41 @@ if(text){
    } 
 }
 else {alert("PLEASE ENTER TEXT")}}
-function changeColor(){this.style.color = "red";
-  
+
+function changeColor(){
+    if(this.parentElement.className=="green"){this.parentElement.className="red"}
+    else{this.parentElement.className="green"}
+   
+  console.log(this.parentElement.className)
 };
-function delet () {
-    mindiv.remove()
+function delet (e) {
+    
+e.target.parentElement.remove(); 
 };
+function factive(){
+    let acitve =document.getElementById("pad").getElementsByTagName("div");
+
+    for(let i=0;i<acitve.length;i++){
+        if(acitve[i].className=="red"){
+            acitve[i].style.display="none"
+        } 
+        else{acitve[i].style.display="flex"}
+    }
+}
+function fshowc(){
+    let comp =document.getElementById("pad").getElementsByTagName("div");
+
+    for(let i=0;i<comp.length;i++){
+        if(comp[i].className=="green"){
+            comp[i].style.display="none"
+        }
+        else{comp[i].style.display="flex"}
+    }
+}
+function fshowa(){
+    let all =document.getElementById("pad").getElementsByTagName("div");
+
+    for(let i=0;i<all.length;i++){
+            all[i].style.display="flex"
+    }
+}
