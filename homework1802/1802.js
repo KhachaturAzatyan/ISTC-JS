f9010500fb109c87fc9cf4bfa00252ca489e0268
Khachatur Azatyan <Khachatur.Azatyan@istc.com>
2019-02-19 19:35:00 +0400

    fin

--- homework1802/1802.js
+++ homework1802/1802.js
@@ -3,24 +3,24 @@ function cliick() {
     var color = document.getElementById("color").value;
     var text = document.getElementById("text").value;
     var background = document.getElementById("background").value;
-    function Create (tag, color, text, background) {
-        this.createTag = document.createElement(tag);
-        this.createTag.className="new";
-        this.creteColor = this.createTag.style.color = color;
-        this.createText = this.createTag.innerText = text;
-        this.creteBackground = this.createTag.style.backgroundColor = background;
-        this.append = function () {
-            document.body.appendChild(this.createTag);
-        };
-        this.append();
-                }
-                if(!tag){
-                    alert("pleas enter tag")
-    };
     var x = new Create (tag, color, text, background);
-        var tag=document.getElementById("tag").value;
-         var color=document.getElementById("color").value ;
-         var text = document.getElementById("text").value ;
-         var background = document.getElementById("background").value ;
-         
+    x.append();
+};
+function Create (tag, color, text, background) {
+        this.tag=tag;
+        this.color=color;
+        this.text=text;
+        this.background=background;
+        this.append = function () {
+        var el = document.createElement(this.tag);
+        el.innerText=this.text;
+        el.className="new";
+        el.style.color=this.color;
+        el.style.backgroundColor=background;
+        document.body.appendChild(el);
+    };
+    
+            }
+            if(!tag){
+                alert("pleas enter tag")
 };
